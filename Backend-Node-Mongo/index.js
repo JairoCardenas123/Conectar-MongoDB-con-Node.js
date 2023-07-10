5a1aad00a0b64acc3b25abe9cf14bc8fe2d9af02
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-07-10 13:27:47 -0500

    PARA MOSTRAR EN EL POSTMAND YA EN JSON

--- Backend-Node-Mongo/index.js
+++ Backend-Node-Mongo/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/config.js";
+import CategoriaRouter from "./routes/categorias.routes.js";
+
 dotenv.config();
 const app = express();
-
+app.use("/categoria",CategoriaRouter);
 const PORT = process.env.PORT;
 
 conectarDB();
---
+++ Backend-Node-Mongo/models/Categorias.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const categoriasSchema = mongoose.Schema(
+    {
+    imagen: {
+        type: String,
+        required:true,
+        trim:true 
+    },
+    nombre:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    descripcion:{
+        type:String,
+        required:true,
+        trim:true
+    }
+    }, 
+    {
+        timesTamps:true,
+    }
+);
+
+const Categoria = mongoose.model("Categoria",categoriasSchema);
+
+export default Categoria;
